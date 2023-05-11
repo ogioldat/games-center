49672acdf45a593c51b6b1d4737df27fc8dec2ef
ogioldat <tomasz.ogiolda00@gmail.com>
2023-05-11 22:48:34 +0200

    Initial Commit

---
+++ GamesCenter/GamesCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesCenterApp.swift
+//  GamesCenter
+//
+//  Created by Tomek Ogiołda on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GamesCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
